8e0dbde582ec08bdcf96a2dd45e0f3bba1989f20
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-07-27 12:00:43 +0300

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,5 @@
+kol_dz = 12
+kol_hours = 1.5
+name_curs = "Python"
+time_for_1_lesson = 1.5 / 12
+print(f"Курс: {name_curs}, всего задач: {kol_dz}, затрачено часов: {kol_hours}, среднее время выполнения: {time_for_1_lesson} часа")
