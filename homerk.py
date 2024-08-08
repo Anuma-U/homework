deff3c1d41b9bf748e6408e915d1aa114274ab6d
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-08-08 23:15:44 +0300

    Add files via upload

--- homerk.py
+++ homerk.py
@@ -1,8 +1,8 @@
-name = "Katerina"
-print(f"Name:  {name}")
-age = 18
-print(f"Age:  {age}")
-age = age + 2
-print("New age: ", age)
-is_student = True
-print("Is Student: ", is_student)
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = list(students)
+students.sort()
+sred_ball = {students[0]: sum(grades[0]) / len(grades[0])}
+for i in range(1, len(students)):
+    sred_ball.update({students[i]: sum(grades[i]) / len(grades[i])})
+print(sred_ball)
