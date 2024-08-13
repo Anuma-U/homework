db3fceb6e3e652630f18ec61cdb2d2e87a5dbc09
Anuma-U <2005.katy.volovlikova@gmail.com>
2024-08-13 20:05:14 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+chislo = int(input("Введите число от 3 до 20 для которого нужен пароль: "))
+proverka_na_povtor = []
+result = ""
+for i in range(1, chislo):
+    for j in range(i, chislo):
+        if chislo % (i + j) == 0 and i != j:
+            if [j, i] not in proverka_na_povtor:
+                proverka_na_povtor.append([i, j])
+                result += f"{i}{j}"
+print(chislo, " - ", result)
